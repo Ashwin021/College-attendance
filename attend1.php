41adf3aec46b724ad58709cbf54a4dc92aeffd23
Ashwin <akashwin0024@gmail.com>
2026-02-27 14:04:16 +0530

    Add initial HTML structure for attendance system

---
+++ attend1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Attendance System</title>
+</head>
+<body>
+<center>
+<h1>Attendance System</h1>
+<a href="attend2.php">
+<button>Add Student</button>
+</a>
+<br><br>
+<a href="attend3.php">
+<button>Mark Daily Attendance</button>
+</a>
+<br><br>
+<a href="attend4.php">
+<button>View Attendance</button>
+</a>
+</center>
+</body>
+</html>
